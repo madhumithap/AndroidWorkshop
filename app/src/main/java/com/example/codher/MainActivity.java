57fc04260d960a4b92c6912c27ce8f1f5a2b0d28
Madhumitha <madhumitha@INDCHN-16280-MPB.local>
2019-02-01 18:01:08 +0530

    Activity lifecycle methods

--- app/src/main/java/com/example/codher/MainActivity.java
+++ app/src/main/java/com/example/codher/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.codher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAIN_ACTIVITY_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(MAIN_ACTIVITY_TAG, "----------------- Creating");
         setContentView(R.layout.activity_main);
         final Button bookButton = findViewById(R.id.bookButton);
         bookButton.setOnClickListener(new View.OnClickListener() {
@@ -17,4 +24,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(MAIN_ACTIVITY_TAG, "----------------- Starting");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(MAIN_ACTIVITY_TAG, "----------------- Resuming");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(MAIN_ACTIVITY_TAG, "----------------- Pausing");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(MAIN_ACTIVITY_TAG, "----------------- Stopping");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(MAIN_ACTIVITY_TAG, "----------------- Destroying");
+    }
+
+
 }
